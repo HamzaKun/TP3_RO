25b51def15096cc947deef15c5d9a3dc34c3dc59
Ace Nanter <ace.nanter@hotmail.fr>
2016-01-14 15:34:34 +0100

    Push de bool

--- recherche_locale.cpp
+++ recherche_locale.cpp
@@ -3,6 +3,8 @@
 
 recherche_locale::recherche_locale(WorkingSolution& ws) : ws_(ws)
 {
+	//dabord cas particulier 2opt* et oropt
+	//puis 2opt* et cross
 }
 
 
@@ -10,7 +12,7 @@ recherche_locale::~recherche_locale()
 {
 }
 
-void recherche_locale::two_opt_etoile_cp() {
+bool recherche_locale::two_opt_etoile_cp() {
 	//Ajout si possible de 2 tournees
 	WorkingSolution new_ws(ws_);
 	RouteInfo* x = new_ws.first();
@@ -27,26 +29,19 @@ void recherche_locale::two_opt_etoile_cp() {
 	}
 }
 
-void recherche_locale::two_opt_etoile() {
+bool recherche_locale::two_opt_etoile() {
 	//inversement de fins de 2 tournees
 }
 
-void recherche_locale::ot_opt_cp() {
+bool recherche_locale::ot_opt_cp() {
 	//Insertion d'un point seul dans une tournee dans une autre tournee
 	//melange au sein d'une meme tournee
 	//swap a et b voisin
 }
-void recherche_locale::ot_opt() {
+bool recherche_locale::ot_opt() {
 	//insertion d'un node dans une nouvelle tournee
 }
 
-
-
-void recherche_locale::cross() {
+bool recherche_locale::cross() {
 	//Echange de 2 points dans 2 tournees differentes
-}
-
-void recherche_locale::recherche_locale_generale() {
-	//dabord cas particulier 2opt* et oropt
-	//puis 2opt* et cross
 }
--- recherche_locale.h
+++ recherche_locale.h
@@ -6,15 +6,14 @@ class recherche_locale
 protected:
 	WorkingSolution& ws_;
 public:
-	recherche_locale(WorkingSolution& ws);
+	recherche_locale(WorkingSolution& ws);		// Appel de tous les algos dans le constructeur
 	~recherche_locale();
 	
 	// Algos RL
-	void two_opt_etoile_cp();					//Cas particulier
-	void two_opt_etoile();						//Cas general (appel cas particulier)
-	void ot_opt_cp();									//Cas particulier
-	void ot_opt();										//Cas general (appel cas particulier)
-	void cross();											//Cas general
-	void recherche_locale_generale();	//Appel de tous les algo
+	bool two_opt_etoile_cp();					//Cas particulier
+	bool two_opt_etoile();						//Cas general (appel cas particulier)
+	bool ot_opt_cp();									//Cas particulier
+	bool ot_opt();										//Cas general (appel cas particulier)
+	bool cross();											//Cas general
 };
 
